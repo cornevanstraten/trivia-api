4d6d58587b7221ad2e9d17465aa75f538e7c43a8
Corné van Straten <corne.straten@array.com>
2023-08-20 21:02:20 -0400

    add basic views and templating

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,18 @@ import (
 
 	"github.com/cornevanstraten/trivia-api/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
 	database.ConnectDb()
-	app := fiber.New()
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
 
 	setupRoutes(app)
 
