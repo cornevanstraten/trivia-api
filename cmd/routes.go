a9de1b0e6023393fe66532f910479b220b990c72
Corné van Straten <corne.straten@array.com>
2023-08-22 17:01:35 -0400

    add show page

--- cmd/routes.go
+++ cmd/routes.go
@@ -9,6 +9,7 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.ListFacts)
 
 	app.Get("/fact", handlers.NewFactView)
-
 	app.Post("/fact", handlers.CreateFact)
+
+	app.Get("/fact/:id", handlers.ShowFact)
 }
